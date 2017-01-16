f7002530ffab8d7756692e9560872ff75efd4f0b
Melissa Munson <melissamunson@Melissas-MacBook-Pro.local>
2017-01-16 13:26:58 -0500

    updated basic to add links to firebase

---
+++ assets/js/week8.js
@@ -0,0 +1,10 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBdt5lhNaTHcvEbf4BmBAqWDvPNBf8gdz4",
+    authDomain: "napnation-week-8.firebaseapp.com",
+    databaseURL: "https://napnation-week-8.firebaseio.com",
+    storageBucket: "napnation-week-8.appspot.com",
+    messagingSenderId: "749153404995"
+  };
+  firebase.initializeApp(config);
